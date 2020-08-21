865e4b6fe0d48cca7490d21077bc0c41e55b06f2
Samuel Turner-Lill <STurner-Lill@PioneerRx.com>
2020-08-21 16:20:18 -0500

    - RegisterDSODll action added to installer

--- SoundboardYourFriends/SoundboardYourFriends.Setup.CustomActions/CustomAction.cs
+++ SoundboardYourFriends/SoundboardYourFriends.Setup.CustomActions/CustomAction.cs
@@ -18,11 +18,7 @@ namespace SoundboardYourFriends.Setup.CustomActions
 
             try
             {
-                session.CustomActionData.Keys.Cast<string>().ToList().ForEach(x => session.Log($"CustomDataKeyValue : {x}"));
-
                 string solutionDirectory = session.CustomActionData["SolutionDirectory"];
-
-                session.Log($"CustomAction InstallVBCable exe path - {solutionDirectory}");
                 string VBCableSetupPath = Path.Combine(Regex.Replace(solutionDirectory,
                     @"SoundboardYourFriends\\SoundboardYourFriends",
                     @"SoundboardYourFriends\\VBCable\\VBCABLE_Setup_x64.exe"));
@@ -34,14 +30,46 @@ namespace SoundboardYourFriends.Setup.CustomActions
                 VBCableProcess.Start();
                 VBCableProcess.WaitForExit();
 
-                session.Log($"CustomAction InstallVBCable completed successfully");
-        }
+                session.Log($"CustomAction InstallVBCable completed successfully - ExitCode({VBCableProcess.ExitCode})");
+            }
             catch (Exception ex)
             {
                 session.Log($"CustomAction InstallVBCable failed - {ex.Message}");
             }
 
             return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult RegisterDSOFileDll(Session session)
+        {
+            session.Log("Begin RegisterDSOFileDll");
+            var actionResult = ActionResult.Success;
+
+            try
+            {
+                string solutionDirectory = session.CustomActionData["SolutionDirectory"];
+                string DSOFileSetupPath = Path.Combine(Regex.Replace(solutionDirectory,
+                    @"SoundboardYourFriends\\SoundboardYourFriends",
+                    @"SoundboardYourFriends\\DSOFile\\DSOFile_Install.bat"));
+
+                var registerDSOFileProcess = new Process();
+                registerDSOFileProcess.StartInfo.FileName = DSOFileSetupPath;
+                registerDSOFileProcess.StartInfo.UseShellExecute = true;
+                registerDSOFileProcess.StartInfo.Verb = "runas";
+                registerDSOFileProcess.Start();
+                registerDSOFileProcess.WaitForExit();
+
+                actionResult = registerDSOFileProcess.ExitCode == 0 ? ActionResult.Success : ActionResult.UserExit;
+                session.Log($"CustomAction RegisterDSOFileDll completed successfully - ExitCode({registerDSOFileProcess.ExitCode})");
+            }
+            catch (Exception ex)
+            {
+                session.Log($"CustomAction RegisterDSOFileDll failed - {ex.Message}");
+                actionResult = ActionResult.Failure;
+            }
+
+            return actionResult;
         }
     }
 }
